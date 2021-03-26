c7446a571161371c45e2b2b118fdfce9d2bd03ce
jabayless <jordanbayless6@gmail.com>
2021-03-25 23:29:51 -0600

    more work on website navbar

--- JavaScript/ResumWebsite/resume-site/src/components/Navbar.js
+++ JavaScript/ResumWebsite/resume-site/src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar(){
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () =>setClick(false);
     return(
         <>
             <nav className='navbar'>
@@ -9,9 +12,51 @@ function Navbar(){
                     <Link to='/' className='navbar-logo'>
                         TRVL <i class="fas fa-satellite"/> 
                     </Link>
-                    <div className='menu-icon'>
+                    <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/education' className='nav-links' onClick={closeMobileMenu}>
+                                Education
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/technologies' className='nav-links' onClick={closeMobileMenu}>
+                                Technologies
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/skills' className='nav-links' onClick={closeMobileMenu}>
+                                Skills
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/certs' className='nav-links' onClick={closeMobileMenu}>
+                                Certs
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
+                                Resume
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact me' className='nav-links' onClick={closeMobileMenu}>
+                                Contact Me
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
